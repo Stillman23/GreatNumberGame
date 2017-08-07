6c9f7a6f649f0c5b2f4e595b273127b7b3604183
Scott Tillman <Scotttillman23@gmail.com>
2017-08-07 17:12:24 -0500

    added randomNumber

--- greatnumbergame.py
+++ greatnumbergame.py
@@ -1,4 +1,5 @@
 from flask import Flask, session, request, redirect, render_template
+import random
 
 
 app = Flask(__name__)
@@ -7,13 +8,13 @@ app.secret_key = 'colab'
 @app.route('/')
 def start():
     if 'random_number' not in session:
-        random_number = session['random_number']
+        session['random_number'] = random.randrange(0,101)
     return render_template('index.html')
 
 @app.route('/guess', methods = ['POST'])
 def guess():
-    if session['random_number'] > request.form['name']:
-        {{'low'}}
+    print session['random_number'] 
+    print request.form['guess']
     return redirect('/')
 
 
